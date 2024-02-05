9efd06cd3c454e733cd23c79ab72ebb580459c2e
Samuel <samuelrivba@gmail.com>
2024-02-05 16:51:09 -0500

    Add final project

---
+++ notification_manager.py
@@ -0,0 +1,34 @@
+import smtplib
+
+from twilio.rest import Client
+
+TWILIO_SID = ""
+TWILIO_AUTH_TOKEN = ""
+TWILIO_VIRTUAL_NUMBER = ""
+TWILIO_VERIFIED_NUMBER = ""
+
+
+class NotificationManager:
+
+    def __init__(self):
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    def send_sms(self, message):
+        message = self.client.messages.create(
+            body=message,
+            from_=TWILIO_VIRTUAL_NUMBER,
+            to=TWILIO_VERIFIED_NUMBER,
+        )
+        # Prints if successfully sent.
+        print(message.sid)
+
+    def send_emails(self, emails, message):
+        with smtplib.SMTP("") as connection:
+            connection.starttls()
+            connection.login("", "")
+            for email in emails:
+                connection.sendmail(
+                    from_addr="",
+                    to_addrs=email,
+                    msg=f"Subject:New Low Price Flight!\n\n{message}".encode('utf-8')
+                )
